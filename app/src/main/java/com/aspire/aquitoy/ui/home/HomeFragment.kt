8afbfd89a540c0bb6e45b779b4ae13393f7440e8
Diego Lopez <74331334+AspireRB@users.noreply.github.com>
2024-04-15 05:51:48 -0500

    Rutas generadas de un punta A a B

--- app/src/main/java/com/aspire/aquitoy/ui/home/HomeFragment.kt
+++ app/src/main/java/com/aspire/aquitoy/ui/home/HomeFragment.kt
@@ -1,15 +1,17 @@
 package com.aspire.aquitoy.ui.home
 
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import com.aspire.aquitoy.R
+import com.aspire.aquitoy.data.ApiService
 import com.aspire.aquitoy.data.LocationService
 import com.aspire.aquitoy.databinding.FragmentHomeBinding
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -17,18 +19,31 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.Polyline
+import com.google.android.gms.maps.model.PolylineOptions
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class HomeFragment : Fragment(), OnMapReadyCallback {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+
     private lateinit var map: GoogleMap
     private val locationService: LocationService = LocationService()
 
+    private var start: String = ""
+    private var end: String = ""
+
+    var poly: Polyline? = null
+
+    @SuppressLint("MissingPermission")
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
+        map.isMyLocationEnabled = true
         createMarker()
     }
 
@@ -44,8 +59,31 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
         val root: View = binding.root
 
         createMapFragment()
+        initListeners()
 
         return root
+    }
+
+    private fun initListeners() {
+        _binding!!.btnService.setOnClickListener {
+            start = ""
+            end = ""
+            poly?.remove()
+            if (poly != null) {
+                poly = null
+            }
+            if (::map.isInitialized) {
+                map.setOnMapClickListener {
+                    if (start.isEmpty()) {
+                        start = "${it.longitude}, ${it.latitude}"
+                    } else if (end.isEmpty()){
+                        end = "${it.longitude}, ${it.latitude}"
+                    } else {
+                        createRoute()
+                    }
+                }
+            }
+        }
     }
 
     override fun onDestroyView() {
@@ -63,13 +101,41 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
             val result = locationService.getUserLocation(requireContext())
             result?.let { location ->
                 val coordinates = LatLng(location.latitude, location.longitude)
-                map.addMarker(MarkerOptions().position(coordinates).title("Ubicación"))
                 map.animateCamera(
                     CameraUpdateFactory.newLatLngZoom(coordinates, 18f),
                     4000,
                     null
                 )
             }
+        }
+    }
+
+    fun getRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://api.openrouteservice.org/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    fun createRoute() {
+        CoroutineScope(Dispatchers.IO).launch {
+            val call = getRetrofit().create(ApiService::class.java).getRoute("5b3ce3597851110001cf62483630a995a4b0492894d42ad8669f8933", start, end,)
+            if (call.isSuccessful) {
+                drawRoute(call.body())
+                Log.i("Aspire", "OK")
+            } else {
+                Log.i("Aspire", "NO OK")
+            }
+        }
+    }
+
+    private fun drawRoute(routeResponse: RouteResponse?) {
+        val polyLineOptions = PolylineOptions()
+        routeResponse?.features?.first()?.geometry?.coordinates?.forEach {
+            polyLineOptions.add(LatLng(it[1], it[0]))
+        }
+        activity?.runOnUiThread {
+            poly = map.addPolyline(polyLineOptions)
         }
     }
 }
